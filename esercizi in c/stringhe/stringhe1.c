2852aec2249ee6bed799ee701aab88540da2a777
lorx08sp <lorx08sp@gmail.com>
2025-05-09 10:05:35 +0000

    rer

--- esercizi in c/stringhe/stringhe1.c
+++ esercizi in c/stringhe/stringhe1.c
@@ -1,16 +1,30 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
+void leggi_stringhe(str[])
 
 int main() 
 {
-    /* 
+    char str[100];
+    
+    leggi_stringhe(str[100]);
+
+
+    /*
     for(char i='A'; i<='Z' ; i++)
     {
         printf("%c  %d\t", i, i);
     }
-        */
+     
     for(int j=0; j<=127; j++)
     {
         printf("%c\t%4d\t%8o\t%8x\n", j, j, j, j);
     }
+    */
+}
+
+void leggi_stringhe(char s[])
+{
+    scanf("%c", s[]);
+    printf("%c", s[])
 }
