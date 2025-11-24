ae18ce9bf516f1c08f2be5ab4aa5af394a8d1741
lorx08sp <lorx08sp@gmail.com>
2025-11-24 08:42:17 +0100

    Create esercizi_file.c

---
+++ esercizi in c/esercizi_file.c
@@ -0,0 +1,51 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+int avv_file();
+int correggi_num();
+
+int main()
+{
+    srand(time(NULL));
+    avv_file();
+    correggi_num();
+    return 0;
+}
+
+int avv_file()
+{
+    FILE *f = fopen("numeri.dat", "wb");
+    if(f == NULL)
+    {
+        printf("err apertura file");
+        return 1;
+    }
+    
+    
+        for(int i=0; i<20; i++)
+        {
+            int r = rand() % 101;
+            fwrite(&r, sizeof(int), 1, f);
+            
+        }
+        
+        fclose(f);
+        return 0;
+}
+
+int correggi_num();
+{
+    FILE *f = fopen("numeri.dat", "ab");
+    if(f == NULL)
+    {
+        printf("err apertura file");
+        return 1;
+    }
+    int nuovo = 42
+    fseek(f, 2 * sizeof(int), SEEK_SET);
+    fwrite(&nuovo, sizeof(int), 1, f);
+    
+    fclose(f)
+    return 0;
+}
